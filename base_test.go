b641c2583c09119ea262df1ca49aaca32b3f88f0
jinguihua <156711203@qq.com>
2023-08-16 10:28:54 +0800

    changed

--- base_test.go
+++ base_test.go
@@ -3,12 +3,13 @@ package myrabbitmq
 import (
 	"fmt"
 	amqp "github.com/rabbitmq/amqp091-go"
+	"os"
 	"testing"
 	"time"
 )
 
 func Test_simpler(t *testing.T) {
-	var addr string = "amqp://19EQqn3TqYdXaudMFGsdcUvUJBsk:dJV4QngHGJRDzy7VqzfUbsSBENrLFrSzLoYTmvne56E@117.41.25.5:5672/"
+	var addr string = os.Getenv("RABBITMQ")
 	var simpleQueue string = "simple"
 	s, err := NewSimple(addr)
 	if err != nil {
@@ -52,7 +53,7 @@ func Test_simpler(t *testing.T) {
 }
 
 func Test_worker(t *testing.T) {
-	var addr string = "amqp://9EQqn3TqYdXaudMFGsdcUvUJBsk:dJV4QngHGJRDzy7VqzfUbsSBENrLFrSzLoYTmvne56E@117.41.25.5:5672/"
+	var addr string = os.Getenv("RABBITMQ")
 	var simpleQueue string = "worker"
 	s, err := NewWorker(addr)
 	if err != nil {
@@ -108,7 +109,7 @@ func Test_worker(t *testing.T) {
 }
 
 func Test_pubScribe(t *testing.T) {
-	var addr string = "amqp://9EQqn3TqYdXaudMFGsdcUvUJBsk:dJV4QngHGJRDzy7VqzfUbsSBENrLFrSzLoYTmvne56E@117.41.25.5:5672/"
+	var addr string = os.Getenv("RABBITMQ")
 	swhName := ""
 	s, err := NewPSubscribe(addr, swhName)
 	if err != nil {
@@ -179,7 +180,7 @@ func Test_pubScribe(t *testing.T) {
 }
 
 func Test_routing(t *testing.T) {
-	var addr string = "amqp://9EQqn3TqYdXaudMFGsdcUvUJBsk:dJV4QngHGJRDzy7VqzfUbsSBENrLFrSzLoYTmvne56E@117.41.25.5:5672/"
+	var addr string = os.Getenv("RABBITMQ")
 	swhName := ""
 	s, err := NewRouting(addr, swhName)
 	if err != nil {
@@ -194,6 +195,77 @@ func Test_routing(t *testing.T) {
 	}
 
 	r2, err := NewRouting(addr, swhName)
+	if err != nil {
+		t.Error("init rabbitmq err: ", err)
+		return
+	}
+
+	go func() {
+		if err := r1.Subscribe("key1", func(d []byte) {
+			fmt.Println("fn1 received: ", string(d))
+		}); err != nil {
+			t.Error("start received fn1 :", err)
+		}
+	}()
+
+	go func() {
+		if err := r2.Subscribe("key2", func(d []byte) {
+			fmt.Println("fn2 received: ", string(d))
+			//time.Sleep(time.Second * 2)
+		}); err != nil {
+			t.Error("start received fn1 :", err)
+		}
+	}()
+
+	go func() {
+		var count int = 0
+		for {
+			if (count & 2) == 0 {
+				tmpstr := fmt.Sprintf("key1 %d %v", count, time.Now().Second())
+				if err := s.Publish("key1", time.Second*5,
+					amqp.Publishing{
+						ContentType: "text/plain",
+						Body:        []byte(tmpstr),
+					}); err != nil {
+					t.Error("create err:", err)
+					break
+				}
+			} else {
+				tmpstr := fmt.Sprintf("key2%d %v", count, time.Now().Second())
+				if err := s.Publish("key2", time.Second*5, amqp.Publishing{
+					ContentType: "text/plain",
+					Body:        []byte(tmpstr),
+				}); err != nil {
+					t.Error("create err:", err)
+					break
+				}
+			}
+			count++
+
+			time.Sleep(time.Millisecond * 400)
+		}
+	}()
+
+	var forever chan struct{}
+	<-forever
+}
+
+func Test_topic(t *testing.T) {
+	var addr string = os.Getenv("RABBITMQ")
+	swhName := ""
+	s, err := NewTopic(addr, swhName)
+	if err != nil {
+		t.Error("init rabbitmq err: ", err)
+		return
+	}
+
+	r1, err := NewTopic(addr, swhName)
+	if err != nil {
+		t.Error("init rabbitmq err: ", err)
+		return
+	}
+
+	r2, err := NewTopic(addr, swhName)
 	if err != nil {
 		t.Error("init rabbitmq err: ", err)
 		return
